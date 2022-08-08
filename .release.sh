fc268965814a73902bb158e5797040e4336c66d9
James Frowen <jamesfrowendev@gmail.com>
2022-08-08 14:36:37 +0100

    fix: fixing version in release

--- .release.sh
+++ .release.sh
@@ -4,7 +4,7 @@ echo "Running release script with [SOURCE_PATH=${SOURCE_PATH}, TARGET_PATH=${TAR
 
 VER=$(echo $1 | sed 's/-[a-z]*//g')
 sed -i -e '/AssemblyVersion/s/\".*\"/\"'$VER'\"/' \
-    source/source/AssemblyInfo.cs
+    source/Runtime/AssemblyInfo.cs
 
 unity-packer pack Mirage.ClientSidePrediction.unitypackage \
     ${SOURCE_PATH} ${TARGET_PATH} \
