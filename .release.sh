34b39a8353c5cc7fdac7c8c306583b97c99f774d
James Frowen <jamesfrowendev@gmail.com>
2022-08-08 14:12:16 +0100

    chore: adding ci [no ci]

---
+++ .release.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Running release script with [SOURCE_PATH=${SOURCE_PATH}, TARGET_PATH=${TARGET_PATH}, args=$@]"
+
+VER=$(echo $1 | sed 's/-[a-z]*//g')
+sed -i -e '/AssemblyVersion/s/\".*\"/\"'$VER'\"/' \
+    source/source/AssemblyInfo.cs
+
+unity-packer pack Mirage.ClientSidePrediction.unitypackage \
+    ${SOURCE_PATH} ${TARGET_PATH} \
+    README.md ${TARGET_PATH}/README.md \
+    CHANGELOG.md ${TARGET_PATH}/CHANGELOG.md
