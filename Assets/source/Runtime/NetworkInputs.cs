9ccb81f96522e44ce6ccab245a62bf1928bcab54
James Frowen <jamesfrowendev@gmail.com>
2022-09-12 21:49:57 +0100

    feat: adding struct to hold current and previous input states

---
+++ Assets/source/Runtime/NetworkInputs.cs
@@ -0,0 +1,23 @@
+﻿/*******************************************************
+ * Copyright (C) 2021 James Frowen <JamesFrowenDev@gmail.com>
+ * 
+ * This file is part of JamesFrowen ClientSidePrediction
+ * 
+ * The code below can not be copied and/or distributed without the express
+ * permission of James Frowen
+ *******************************************************/
+
+namespace JamesFrowen.CSP
+{
+    public struct NetworkInputs<TInput>
+    {
+        public TInput Current;
+        public TInput Previous;
+
+        public NetworkInputs(TInput current, TInput previous)
+        {
+            Current = current;
+            Previous = previous;
+        }
+    }
+}
