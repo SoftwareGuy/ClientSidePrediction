2f47a3fc3e045f8f5c62bdbb1a08d2734f4a2c07
Matt Coburn <coburn@dandere.net>
2023-02-08 12:19:56 +1000

    Unity 2023.1 only has the new Physics.simulationMode thing

--- Assets/source/Runtime/DefaultPredictionSimulation.cs
+++ Assets/source/Runtime/DefaultPredictionSimulation.cs
@@ -27,7 +27,7 @@ namespace JamesFrowen.CSP.Simulations
             switch (mode)
             {
                 case SimulationMode.Physics3D:
-#if UNITY_2021_3_OR_NEWER
+#if UNITY_2023_1_OR_NEWER
                     Physics.simulationMode = UnityEngine.SimulationMode.Script;
 #else
                     Physics.autoSimulation = false;
@@ -61,7 +61,8 @@ namespace JamesFrowen.CSP.Simulations
             switch (mode)
             {
                 case SimulationMode.Physics3D:
-#if UNITY_2021_3_OR_NEWER
+#if UNITY_2023_1_OR_NEWER
+                    // Tell unity to update it by script only.
                     Physics.simulationMode = UnityEngine.SimulationMode.Script;
 #else
                     Physics.autoSimulation = false;
